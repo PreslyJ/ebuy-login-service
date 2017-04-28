9eebd0820229896268b3dd26b954a2e304e96fee
unknown <presly>
2017-04-28 11:38:45 +0530

    change redirect url for logout

--- src/main/java/com/ctf/sims/login/controller/LoginController.java
+++ src/main/java/com/ctf/sims/login/controller/LoginController.java
@@ -6,6 +6,9 @@ import java.util.Base64;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +21,8 @@ import com.ctf.sims.login.model.UserLogin;
 @RestController
 public class LoginController  {
 	
+	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+		
 	@SuppressWarnings("rawtypes")
 	@ModelAttribute
 	@CrossOrigin
@@ -52,7 +57,9 @@ public class LoginController  {
 		
 		RedirectView redirectView = new RedirectView();
 		
-		redirectView.setUrl(request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() );
+		log.error(request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort());
+		
+		redirectView.setUrl("192.168.1.50:8086");
 	    
 		return redirectView;
 
