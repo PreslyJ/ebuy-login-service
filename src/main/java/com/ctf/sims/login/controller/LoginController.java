017bf1d69ee532d63d11739fab3f082f2cfc0919
unknown <presly>
2017-05-02 15:51:38 +0530

    utf-8  encoded cookie name

--- src/main/java/com/ctf/sims/login/controller/LoginController.java
+++ src/main/java/com/ctf/sims/login/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.ctf.sims.login.controller;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.Base64;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -33,10 +34,17 @@ public class LoginController  {
 			
 			String encodedStr=Base64.getEncoder().encodeToString(enc.getBytes());
 			
-			String safeCookieName = URLEncoder.encode("nginxauth", "UTF-8");
-			String safeCookieValue = URLEncoder.encode(encodedStr, "UTF-8");
+			byte[] byteText = encodedStr.getBytes(Charset.forName("UTF-8"));
+			//To get original string from byte.
+			String originalString= new String(byteText , "UTF-8");
 			
-			Cookie cookie = new Cookie(safeCookieName, safeCookieValue);
+			
+			//Charset.forName("UTF-8").encode(encodedStr);
+		/*	
+			String safeCookieName = URLEncoder.encode("nginxauth", "UTF-8");
+			String safeCookieValue = URLEncoder.encode(encodedStr, "UTF-8");*/
+			
+			Cookie cookie = new Cookie("nginxauth", originalString);
 			cookie.setHttpOnly(true);
    
 			log.error("Cookie VAL==>   "+cookie.getValue());
