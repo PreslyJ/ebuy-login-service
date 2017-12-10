fa31b239181c18260bc65c01ad116120e280ae23
presly <presly>
2017-12-10 08:44:46 +0530

    build

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+git pull origin master
+git submodule foreach git checkout master &&  git pull origin master
+mvn clean install
 docker build --force-rm -t node1:5000/ebuy-login-service .
 docker push node1:5000/ebuy-login-service
